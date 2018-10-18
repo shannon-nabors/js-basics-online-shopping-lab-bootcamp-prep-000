72480338bc085322d3414522b789570e261ea751
Shannon Nabors <shannon.nabors@gmail.com>
2018-10-18 00:48:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -53,6 +53,8 @@ function placeOrder(cardNumber) {
   if(!!cardNumber) {
     cart = [];
     return("Your total cost is $" + total() + ", which will be charged to the card " + cardNumber + ".");
+  } else {
+    return("Sorry, we don't have a credit card on file for you.");
   }
 }
 
