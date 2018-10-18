a59519273156bb1e9cafd8bd4726350b0cac8125
Shannon Nabors <shannon.nabors@gmail.com>
2018-10-18 00:50:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if(!!cardNumber) {
     cart = [];
-    return("Your total cost is $" + total() + ", which will be charged to the card " + cardNumber + ".");
+    return("Your total cost is $" + t + ", which will be charged to the card " + cardNumber + ".");
   } else {
     return("Sorry, we don't have a credit card on file for you.");
   }
