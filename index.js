3246e330165fbca78939b270c2b141f6748c1bf3
Shannon Nabors <shannon.nabors@gmail.com>
2018-10-18 00:39:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,4 +58,4 @@ console.log(addToCart('soy sauce'));
 console.log(addToCart('bacon'));
 console.log(viewCart());
 console.log(total());
-console.log(removeFromCart('soy sauce'));
+console.log(removeFromCart('bacon'));
